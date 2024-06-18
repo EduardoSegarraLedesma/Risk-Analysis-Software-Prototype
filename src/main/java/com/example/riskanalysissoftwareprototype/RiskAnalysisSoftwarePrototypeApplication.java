2b6f2de186c3b7bc2ccbac33a703428f328a3daa
EduardoSegarraLedesma <eduardo.segarra.ledesma@alumnos.upm.es>
2024-06-19 00:31:40 +0800

    bug fix empty project name

--- src/main/java/com/example/riskanalysissoftwareprototype/RiskAnalysisSoftwarePrototypeApplication.java
+++ src/main/java/com/example/riskanalysissoftwareprototype/RiskAnalysisSoftwarePrototypeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RiskAnalysisSoftwarePrototypeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RiskAnalysisSoftwarePrototypeApplication.class, args);
     }
-
 }
